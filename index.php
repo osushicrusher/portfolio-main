615347165071a1bf69a143174cf39d78ae731045
osushicrusher <osushicrusher@gmail.com>
2020-03-06 14:33:00 +0900

    'master'

--- index.php
+++ index.php
@@ -1,5 +1,58 @@
 
-<?php include_once("index.html"); ?>
+<?php include_once("index.html");
+
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    // POSTでのアクセスでない場合
+    $name = '';
+    $email = '';
+    $subject = '';
+    $message = '';
+    $err_msg = '';
+    $complete_msg = '';
+
+} else {
+    // フォームがサブミットされた場合（POST処理）
+    // 入力された値を取得する
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    // エラーメッセージ・完了メッセージの用意
+    $err_msg = '';
+    $complete_msg = '';
+
+    // 空チェック
+    if ($name == '' || $email == '' || $subject == '' || $message == '') {
+        $err_msg = '全ての項目を入力してください。';
+    }
+
+    // エラーなし（全ての項目が入力されている）
+    if ($err_msg == '') {
+        $to = 'osushicrusher@gmail.com'; // 管理者のメールアドレスなど送信先を指定
+        $headers = "From: " . $email . "\r\n";
+
+        // 本文の最後に名前を追加
+        $message .= "\r\n\r\n" . $name;
+
+        // メール送信
+        mb_send_mail($to, $subject, $message, $headers);
+
+        // 完了メッセージ
+        $complete_msg = '送信されました！';
+
+        // 全てクリア
+        $name = '';
+        $email = '';
+        $subject = '';
+        $message = '';
+    }
+
+}
+
+
+
+?>
 
 <!DOCTYPE html>
 <html lang="ja">
