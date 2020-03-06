467337685b72e6ac0de9d3548f384179e1a9f6c9
osushicrusher <osushicrusher@gmail.com>
2020-03-06 14:47:25 +0900

    'master'

--- contact.php
+++ contact.php
@@ -66,17 +66,17 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST') {
         </p>
         <p class="contact__form">
             <label for="email">メールアドレス <span class="required">必須</span></label><br>
-            <input type="email" name="email" id="email" class="contact__input contact__input--text" placeholder="例) yoshino@example.com" value="<?php echo $email ?>" required>
+            <input type="email" name="email" id="email" class="contact__input contact__input--text" placeholder="例) yoshino@example.com" value="<?php echo $email; ?>" required>
         </p>                            
         <p class="contact__form">                            
             <label for="subject">件名 </label><br>
-            <input type="text" id="subject" name="subject" class="contact__input contact__input--text" placeholder="件名" id="subject" value="<?php echo $subject ?>">
+            <input type="text" id="subject" name="subject" class="contact__input contact__input--text" placeholder="件名" id="subject" value="<?php echo $subject; ?>">
         </p>
         <p class="contact__form">
             <label for="message">お問い合わせ内容 <span class="required">必須</span></label><br>
             <textarea name="message" class="contact__input contact__input--message" id="message" required><?php echo $message; ?></textarea>
         </p>                            
-        <button type="submit" value="入力を確認する" name="btn_confirm" class="btn-square-shadow"></button>
+        <button type="submit" value="入力を確認する" name="btn_confirm" class="btn-square-shadow">送信する</button>
     </form>
 </body>
 </html>
