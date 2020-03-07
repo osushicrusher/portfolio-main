d8630169be87ee2b70c26364485f051492237600
osushicrusher <osushicrusher@gmail.com>
2020-03-07 13:02:35 +0900

    'master'

--- contact.php
+++ contact.php
@@ -2,6 +2,26 @@
 
 include('index.php');
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $to = 'osushicrusher@gmail.com';
+        $headers = "From: " . $email . "\r\n";
+
+        // 本文の最後に名前を追加
+        $message .= "\r\n\r\n" . $name;
+
+        // メール送信
+        mb_send_mail($to, $subject, $message, $headers);
+
+        // 完了メッセージ
+        $complete_msg = '送信されました';
+
+        // 全てクリア
+        $name = '';
+        $email = '';
+        $subject = '';
+        $message = '';
+}
+
 ?>
 
 <!DOCTYPE html>
--- index.php
+++ index.php
@@ -15,28 +15,6 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST') {
     $email = $_POST['email'];
     $subject = $_POST['subject'];
     $message = $_POST['message'];
-
-
-    // エラーなし(全ての項目が入力されている)
-    if ($name != '' || $email != '' || $subject != '' || $message != '') {
-        $to = 'osushicrusher@gmail.com';
-        $headers = "From: " . $email . "\r\n";
-
-        // 本文の最後に名前を追加
-        $message .= "\r\n\r\n" . $name;
-
-        // メール送信
-        mb_send_mail($to, $subject, $message, $headers);
-
-        // 完了メッセージ
-        $complete_msg = '送信されました';
-
-        // 全てクリア
-        $name = '';
-        $email = '';
-        $subject = '';
-        $message = '';
-    }
 }
 
 ?>
