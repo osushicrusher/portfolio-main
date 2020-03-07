3644dbc20e3b8525a4e9b8d2328ffef9d9dde027
osushicrusher <osushicrusher@gmail.com>
2020-03-07 14:25:33 +0900

    'master'

--- contact.php
+++ contact.php
@@ -1,26 +1,3 @@
-<?php
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $to = 'osushicrusher@gmail.com';
-        $headers = "From: " . $email . "\r\n";
-
-        // 本文の最後に名前を追加
-        $message .= "\r\n\r\n" . $name;
-
-        // メール送信
-        mb_send_mail($to, $subject, $message, $headers);
-
-        // 完了メッセージ
-        $complete_msg = '送信されました';
-
-        // 全てクリア
-        $name = '';
-        $email = '';
-        $subject = '';
-        $message = '';
-}
-
-?>
 
 <!DOCTYPE html>
 <html lang="ja">
@@ -31,9 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <title>Contact</title>
 </head>
 <body>
-    <?php if ($complete_msg != ''): ?>
-        <?php echo $complete_msg; ?>
-    <?php endif; ?>
+
 
     <form class="contact__form-box" method="POST">
         <p class="contact__form">                            
--- index.php
+++ index.php
@@ -1,39 +1,7 @@
 
 <?php
 
-if ($_SERVER['REQUEST_METHOD'] != 'POST') {
-    // POST処理の場合
-    $name = '';
-    $email = '';
-    $subject = '';
-    $message = '';
-    $complete_msg = '';
-} else {
-    // formがsubmitされた場合(POST処理)
-    // 入力された値を取得する
-    $name = $POST['name'];
-    $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
-
-    $to = 'osushicrusher@gmail.com';
-    $headers = "From: " . $email . "\r\n";
-
-    // 本文の最後に名前を追加
-    $message .= "\r\n\r\n" . $name;
-
-    // メール送信
-    mb_send_mail($to, $subject, $message, $headers);
-
-    // 完了メッセージ
-    $complete_msg = '送信されました';
-
-    // 全てクリア
-    $name = '';
-    $email = '';
-    $subject = '';
-    $message = '';
-}
+var_dump($_POST);
 
 ?>
 
@@ -186,31 +154,24 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST') {
             <div class="heading-secondary-box">
                 <a id="contact"><h2 class="heading-secondary">Contact</h2></a>
             </div>
-
-            <?php if ($complete_msg != ''): ?>
-                <div>
-                    <?php echo $complete_msg; ?>
-                </div>
-            <?php endif; ?>
-
-            <form class="contact__form-box" method="post">
+            <form class="contact__form-box" method="POST">
                 <p class="contact__form">                            
                     <label for="name">お名前</label><br>
-                    <input type="text" name="name" class="contact__input contact__input--text" id="name" placeholder="例）田中 太郎" value="<?php echo $name; ?>" required>
+                    <input type="text" name="name" class="contact__input contact__input--text" id="name" placeholder="例）田中 太郎" value="" required>
                 </p>
                 <p class="contact__form">
                     <label for="email">メールアドレス</label><br>
-                    <input type="email" name="email" id="email" class="contact__input contact__input--text" placeholder="例) yoshino@example.com" value="<?php echo $email; ?>" required>
+                    <input type="email" name="email" id="email" class="contact__input contact__input--text" placeholder="例) yoshino@example.com" value="" required>
                 </p>                            
                 <p class="contact__form">                            
                     <label for="subject">件名</label><br>
-                    <input type="text" name="subject" id="subject" class="contact__input contact__input--text" placeholder="件名" id="subject" value="<?php echo $subject; ?>" required>
+                    <input type="text" name="subject" id="subject" class="contact__input contact__input--text" placeholder="件名" id="subject" value="" required>
                 </p>
                 <p class="contact__form">
                     <label for="message">お問い合わせ内容</label><br>
-                    <textarea name="message" class="contact__input contact__input--message" id="message" required><?php echo $message; ?></textarea>
+                    <textarea name="message" class="contact__input contact__input--message" id="message" required></textarea>
                 </p>                            
-                <button type="submit" name="btn_confirm" class="btn-square-shadow">送信する</button>
+                <input type="submit" name="btn_confirm" class="btn-square-shadow" value="入力内容を確認する">
             </form>
         </section>
     </main>
