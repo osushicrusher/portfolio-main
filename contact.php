b1c47a9b1385f063db535c6d7bb98e8040aec6c1
osushicrusher <osushicrusher@gmail.com>
2020-03-07 15:04:58 +0900

    'master'

--- contact.php
+++ contact.php
@@ -1,7 +1,6 @@
 
 <?php
 
-if (!empty($_POST['btn_confirm'])) {
     session_start();
 
     $name = $_SESSION['name'];
@@ -9,22 +8,6 @@ if (!empty($_POST['btn_confirm'])) {
     $subject = $_SESSION['subject'];
     $message = $_SESSION['message'];
 
-    $to = 'osushicrusher@gmail.com'; // 管理者のメールアドレスなど送信先を指定
-    $headers = "From: " . $email . "\r\n";
-
-    // 本文の最後に名前を追加
-    $message .= "\r\n\r\n" . $name;
-
-    // メール送信
-    mb_send_mail($to, $subject, $message, $headers);
-
-    // 全てクリア
-    $name = '';
-    $email = '';
-    $subject = '';
-    $message = '';
-
-}
 
 ?>
 
