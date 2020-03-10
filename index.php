9297e921de579e7c204c147de18538ea05a67515
osushicrusher <osushicrusher@gmail.com>
2020-03-10 15:42:49 +0900

    'master'

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Yoshino</title>
+    <title>Hikaru Yoshino</title>
 </head>
 <body> 
     <header class="header">
