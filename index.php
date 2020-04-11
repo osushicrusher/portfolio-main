2a66b59f2194e6f30353ba025964a452cae0a4cb
osushicrusher <osushicrusher@gmail.com>
2020-04-11 22:25:48 +0900

    metaの修正

--- index.php
+++ index.php
@@ -42,7 +42,9 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hikaru Yoshino</title>
+    <meta name="format-detection" content="telephone=no">
+    <title>吉野光のポートフォリオ</title>
+    <meta content="吉野光のポートフォリオ" name="title">
 </head>
 <body> 
     <div class="wrapper">
